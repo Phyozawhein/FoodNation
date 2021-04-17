55e60068dcf92acde62547924b51bd68c2cce2c0
PhyoZawHein <phyozawhein1997@gmail.com>
2021-04-17 14:26:26 -0400

    adding Authentication & Firebase configuration

--- fod_frontend/src/App.js
+++ fod_frontend/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './App.module.css';
+import SignUp from './components/SignUp/SignUp'
+import {Container} from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="appHeader">
+
       </header>
+        <Container className="d-felx align-items-center">
+          <SignUp/>
+        </Container>
     </div>
   );
 }
