3463a00307a95bc1b057953f79d421df62337b02
PETERYE100 <pye000@citymail.cuny.edu>
2021-04-18 15:40:00 -0400

    profile page template created, still needs dynamic text and info from firebase"

--- fod_frontend/src/App.js
+++ fod_frontend/src/App.js
@@ -6,6 +6,8 @@ import Login from './components/Login/Login'
 import { AuthProvider } from './context/AuthContext';
 import Dashboard from './components/Dashboard/Dashboard';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Profile from './components/Profile/Profile';
+
 function App() {
   return (
     <div className="App">
@@ -15,6 +17,7 @@ function App() {
             <Route  path="/signup" component={SignUp}/>
             <Route path="/login" component={Login}/>
             <Route exact path ="/" component={Dashboard}/>
+            <Route exact path ="/profile" component={Profile}/>
             
         </Switch>
       </AuthProvider>
