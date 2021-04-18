f35ec8f8642c0e2fbb2059fec0bb431a4e0ed914
PhyoZawHein <phyozawhein1997@gmail.com>
2021-04-17 21:12:31 -0400

    finished sign up page

--- fod_frontend/src/components/SignUp/SignUp.js
+++ fod_frontend/src/components/SignUp/SignUp.js
@@ -1,9 +1,10 @@
 import React, {useRef, useState} from 'react'
-import {Card, Form, Button, Alert} from 'react-bootstrap';
+import {Container, Card, Row, Form, Button, Alert} from 'react-bootstrap';
 import {useAuth} from '../../context/AuthContext';
 import Fire from '../../firebase.config';
 import classes from './SignUp.module.css';
 import {Link} from 'react-router-dom';
+import {ReactComponent as Logo} from '../../assets/FN-Logo.svg';
 
 export default function SignUp() {
 
@@ -57,7 +58,7 @@ export default function SignUp() {
     }   
 
     const UserPage= (
-        <Form.Row>
+        <>
             <Form.Group id="email">
                 <Form.Label>
                     Username
@@ -76,11 +77,11 @@ export default function SignUp() {
                 </Form.Label>
                 <Form.Control type="text" ref={lNameRef} required/>
             </Form.Group>
-        </Form.Row>
+        </>
     )
 
     const CharityPage=(
-    <Form.Row>
+    <>
         <Form.Group id="cid">
             <Form.Label>
                 Charity Identity Number (CIN)
@@ -93,18 +94,18 @@ export default function SignUp() {
             </Form.Label>
             <Form.Control type="text" ref={oNameRef} required/>
         </Form.Group>
-    </Form.Row>
+    </>
     )
 
     const RestaurantPage =(
-        <Form.Row>
+        <>
         <Form.Group id="restaurantName">
             <Form.Label>
                 Restaurant Name
             </Form.Label>
             <Form.Control type="text" ref={rNameRef} required/>
         </Form.Group>
-    </Form.Row>
+    </>
 
     )
 
@@ -121,9 +122,13 @@ export default function SignUp() {
 
     return (
         <>
+        <Container className="d-flex align-items-center justify-content-center" style={{minHeight: "100vh"}}>
+            <div className="w-100" style={{maxWidth: "500px"}}>
             <Card className={`${classes.container} ${classes.font}`}>
                 <Card.Body>
-                <h2 className="text-center mb-4">Sign Up</h2>
+                <Row className="d-flex align-items-center justify-content-center">
+                   <Logo className={classes.logo}/>
+                </Row>
                 {error &&<Alert variant="danger">{error}</Alert>}
                 
                 <Form onSubmit={handleSubmit} > 
@@ -141,13 +146,13 @@ export default function SignUp() {
                         <Form.Control type="password" ref={passwordConfirmRef} required/>
                     </Form.Group>
                     <Form.Row>
-                        <Form.Group id="email">
+                        <Form.Group  className={classes.contact}>
                             <Form.Label>
                                 Email
                             </Form.Label>
                             <Form.Control type="email" ref={emailRef} required/>
                         </Form.Group>
-                        <Form.Group>
+                        <Form.Group className={classes.contact} >
                             <Form.Label>Phone</Form.Label>
                             <Form.Control type="text" ref={phoneNumberRef} required />
                         </Form.Group>
@@ -188,9 +193,56 @@ export default function SignUp() {
                 </Form>
                 </Card.Body>
             </Card>
-            <div className= "w-100 text-center mt-2 color-white">
+            <div className= {`${classes.font} w-100 text-center mt-2`}>
                 Already have an account ? <Link to="/login">Login</Link>
             </div>
+            </div>
+            </Container>
         </>
     )
 }
+
+
+
+//     return (
+//         <>
+//         <Container className="d-flex align-items-center justify-content-center" style={{minHeight: "100vh"}}>
+//             <div className="w-100" style={{maxWidth: "400px"}}>
+//             <Card className={`${classes.container} ${classes.font}`}>
+//                 <Card.Body>
+//                 <Row className="d-flex align-items-center justify-content-center">
+//                     <Logo className={classes.logo}/>
+//                 </Row>
+//                 {error &&<Alert variant="danger">{error}</Alert>}
+                
+//                 <Form onSubmit={handleSubmit} >
+//                     <Form.Group id="email">
+//                         <Form.Label>
+//                             Email
+//                         </Form.Label>
+//                         <Form.Control type="email" ref={emailRef} required/>
+//                     </Form.Group>
+//                     <Form.Group id="password">
+//                         <Form.Label>
+//                             Password
+//                         </Form.Label>
+//                         <Form.Control type="password" ref={passwordRef} required/>
+//                     </Form.Group>
+//                     <Form.Group id="password-confirm">
+//                         <Form.Label>
+//                             Password Confirmation
+//                         </Form.Label>
+//                         <Form.Control type="password" ref={passwordConfirmRef} required/>
+//                     </Form.Group>
+//                     <Button type="submit" className={`${classes.submitbutton} w-100`} disabled={loading}>Sign Up</Button>
+//                 </Form>
+//                 </Card.Body>
+//             </Card>
+//             <div className= {`${classes.font} w-100 text-center mt-2`}>
+//                 Already have an account ? Login
+//             </div>
+//             </div>
+//         </Container>
+//         </>
+//     )
+// }
