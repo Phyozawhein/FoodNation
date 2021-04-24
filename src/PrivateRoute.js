7947b1c2f958c1582da9c9c3e05075733b3ebc4f
PhyoZawHein <phyozawhein1997@gmail.com>
2021-04-24 14:59:37 -0400

    making some adjustments after merging

--- src/PrivateRoute.js
+++ src/PrivateRoute.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
 import { useAuth } from './context/AuthContext';
 
-<<<<<<< HEAD
 function PrivateRoute({ component: Component, ...rest }) {
   const { currentUser } = useAuth();
   console.log(currentUser);
@@ -14,20 +13,6 @@ function PrivateRoute({ component: Component, ...rest }) {
       }}
     />
   );
-=======
-function PrivateRoute({component: Component, ...rest}) {
-    const {currentUser} = useAuth();
-    
-    return (
-        <Route
-            {...rest}
-            render = {props => {
-               return currentUser ? <Component {...props} /> : <Redirect to ="/login" />
-            }}
-        >
-        </Route>
-    )
->>>>>>> a94cd5d311c820153987b0bae1af2e0a2cb07593
 }
 
 export default PrivateRoute;
