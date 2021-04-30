9154037313a13faa7893beb915f2cf674a439a0d
PhyoZawHein <phyozawhein1997@gmail.com>
2021-04-29 21:04:22 -0400

    Fixed some issues

--- src/components/Donation/Donation.js
+++ src/components/Donation/Donation.js
@@ -1,185 +1,166 @@
-import styles from './Donation.module.scss'
-import React, {useState, useRef,useEffect} from 'react'
-import {useAuth} from '../../context/AuthContext';
+import styles from './Donation.module.css';
+import React, { useState, useRef, useEffect } from 'react';
+import { useAuth } from '../../context/AuthContext';
 import Fire from '../../firebase.config';
-import {Button,Form, Alert} from 'react-bootstrap';
+import { Button, Form, Alert } from 'react-bootstrap';
 
-function Donation () {
+function Donation() {
+  let db = Fire.db;
+  const user = useAuth().currentUser.email;
+  const ResName = useRef();
+  const Address = useRef();
+  const ItemLists = useRef();
+  const date = useRef();
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState('');
+  const [view, setView] = useState(false);
+  const [array, setArray] = useState([]);
+  const [id, setId] = useState('');
+  const [orgName, setOrgName] = useState('');
 
-    let db = Fire.db
-    const user = useAuth().currentUser.email
-    const ResName   = useRef()
-    const Address   = useRef()
-    const ItemLists = useRef()
-    const date      = useRef()
-    const [error,setError]=useState('');
-    const [success,setSuccess]=useState('');
-    const [view,setView]=useState(false);
-    const [array,setArray]=useState([]);
-    const [id, setId]=useState('');
-    const [orgName, setOrgName]=useState('');
+  async function handleSubmit(e) {
+    e.preventDefault();
 
-    async function handleSubmit(e){
-        e.preventDefault();
-   
-        try{
-            
-            db.getCollection('Donation').doc().set({
-                
-                resName: ResName.current.value,
-                orgName: orgName,                         
-                address: Address.current.value,
-                itemLists:ItemLists.current.value,
-                date: date.current.value,
-                orgid: id,
-                resid: ResName.current.value.toString().toLowerCase().replaceAll(" ","-"),
-                
-            }).then(response=>{
-                setSuccess("Donation appointment created");
-                
-                
-            }).catch(error=>{
-
-                console.log(error);
-                 setError(error.message)
-            } 
-                 );
-        }
-        catch(err){
-            console.log(err);
-            setError('');
-            setError(err.message);
-        }
-    }
-
-    useEffect(() => {
-
-        db.getCollection("Users").where("email", "==", user)
-        .get()
-        .then((snapShotQuery) => {
-
-            let typeCheck = snapShotQuery.docs.filter(doc => doc.data().type === "restaurant").length;
-            
-            if (typeCheck === 1) {
-                setView(true);
-            }
-
-        }).catch(error => {
-
-            console.log(error);
-             setError(error.message)
+    try {
+      db.getCollection('Donation')
+        .doc()
+        .set({
+          resName: ResName.current.value,
+          orgName: orgName,
+          address: Address.current.value,
+          itemLists: ItemLists.current.value,
+          date: date.current.value,
+          orgid: id,
+          resid: ResName.current.value.toString().toLowerCase().replaceAll(' ', '-'),
         })
-
-        let array =[]
-        db.getCollection("CharityDetails").get().then((querySnapshot) => {
-            querySnapshot.forEach((doc) => {
-               
-                array.push(doc.data().orgName);
-            }
-            );
-                setArray(array);
+        .then((response) => {
+          setSuccess('Donation appointment created');
+        })
+        .catch((error) => {
+          console.log(error);
+          setError(error.message);
         });
-
-
-    },[])
-
-    function updateId() {
-        let zone = document.getElementById("idselect")
-        
-        if (zone && zone.value !=null){
-            setOrgName(zone.value);                      // FIX ME
-            setId(zone.value.toString().toLowerCase().replaceAll(" ","-"));
-        }
+    } catch (err) {
+      console.log(err);
+      setError('');
+      setError(err.message);
     }
+  }
 
-    const viewPage = (
-        <div className={styles.rectangle}>
-            
-            <div>
-                <h1 className={styles.headline}>
-                    Schedule a Donation
-                </h1>
-            </div>
-        {success &&<Alert variant="success">{success}</Alert>}
+  useEffect(() => {
+    db.getCollection('Users')
+      .where('email', '==', user)
+      .get()
+      .then((snapShotQuery) => {
+        let typeCheck = snapShotQuery.docs.filter((doc) => doc.data().type === 'restaurant').length;
 
-        <Form onSubmit={handleSubmit}>
+        if (typeCheck === 1) {
+          setView(true);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        setError(error.message);
+      });
 
-            <Form.Group id="resName">
-                <Form.Label className={styles.label}>
-                    Restaurant Name
-                    <br/>
-                </Form.Label>
-                <Form.Control className = "input" type="text" ref={ResName} required/>
-            </Form.Group>
-                    <br/>
-            <Form.Group id="orgName">
-            <Form.Label className={styles.label}>
-                    Organization Name
-                    <br/>
-                </Form.Label>
+    let array = [];
+    db.getCollection('CharityDetails')
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          array.push(doc.data().orgName);
+        });
+        setArray(array);
+      });
+  }, []);
 
-                <select id="idselect" class="form-select" className={styles.label1} onChange={updateId}>
-                    <option selected>Choose a Charity</option>
-                    
-                    {array.map(item=> ( 
-                    <option value={item}>
-                         {item}
-                    </option>))}
-                
-                </select>
-            </Form.Group>
-                    <br/>
-            <Form.Group id="address">
-                <Form.Label className={styles.label}>
-                    Address
-                    <br/>
-                </Form.Label>
-                <Form.Control className = "input" type="text" ref={Address} required/>
-            </Form.Group>
-                    <br/>
-            <Form.Group id="itemLists">
-                <Form.Label className={styles.label}>
-                    Item Lists
-                    <br/>
-                </Form.Label>
-                <Form.Control className = "input1" type="textarea" ref={ItemLists} required/>
-            </Form.Group>
-                    <br/>
-                    <br/>
-                    
-            <Form.Group id="date">
-                <Form.Label className={styles.label}>
-                    Choose a Date
-                    <br/>
-                    <br/>
-                </Form.Label>
-                <Form.Control className="date" type="datetime-local" ref={date} required/>
-            </Form.Group>
-            <br/>
-            <Button type="post" className={styles.postbutton}>
-                Post
-            </Button>
-        </Form>
+  function updateId() {
+    let zone = document.getElementById('idselect');
 
-    
-        </div>    
-            )
+    if (zone && zone.value != null) {
+      setOrgName(zone.value); // FIX ME
+      setId(zone.value.toString().toLowerCase().replaceAll(' ', '-'));
+    }
+  }
 
-    const cantViewPage = (
-        <div>
-            <h1 className="cantView">Not authorized to view this page</h1>
-        </div>
-    )
+  const viewPage = (
+    <div className={styles.rectangle}>
+      <div>
+        <h1 className={styles.headline}>Schedule a Donation</h1>
+      </div>
+      {success && <Alert variant="success">{success}</Alert>}
 
-    return (
-        <div>
-             
-        {error &&<Alert variant="danger">{error}</Alert>}
- 
-        { view === true ?  viewPage   :  cantViewPage }
+      <Form onSubmit={handleSubmit}>
+        <Form.Group id="resName">
+          <Form.Label className={styles.label}>
+            Restaurant Name
+            <br />
+          </Form.Label>
+          <Form.Control className="input" type="text" ref={ResName} required />
+        </Form.Group>
+        <br />
+        <Form.Group id="orgName">
+          <Form.Label className={styles.label}>
+            Organization Name
+            <br />
+          </Form.Label>
 
+          <select id="idselect" class="form-select" className={styles.label1} onChange={updateId}>
+            <option selected>Choose a Charity</option>
+
+            {array.map((item) => (
+              <option value={item}>{item}</option>
+            ))}
+          </select>
+        </Form.Group>
+        <br />
+        <Form.Group id="address">
+          <Form.Label className={styles.label}>
+            Address
+            <br />
+          </Form.Label>
+          <Form.Control className="input" type="text" ref={Address} required />
+        </Form.Group>
+        <br />
+        <Form.Group id="itemLists">
+          <Form.Label className={styles.label}>
+            Item Lists
+            <br />
+          </Form.Label>
+          <Form.Control className="input1" type="textarea" ref={ItemLists} required />
+        </Form.Group>
+        <br />
+        <br />
+
+        <Form.Group id="date">
+          <Form.Label className={styles.label}>
+            Choose a Date
+            <br />
+            <br />
+          </Form.Label>
+          <Form.Control className="date" type="datetime-local" ref={date} required />
+        </Form.Group>
+        <br />
+        <Button type="post" className={styles.postbutton}>
+          Post
+        </Button>
+      </Form>
     </div>
-    )
+  );
+
+  const cantViewPage = (
+    <div>
+      <h1 className="cantView">Not authorized to view this page</h1>
+    </div>
+  );
+
+  return (
+    <div>
+      {error && <Alert variant="danger">{error}</Alert>}
+
+      {view === true ? viewPage : cantViewPage}
+    </div>
+  );
 }
 
 export default Donation;
