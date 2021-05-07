8dc6a9d725a849506ed5627451bf12035f73ca17
jamesey123 <jameseyexcaliber@gmail.com>
2021-05-06 21:35:59 -0400

    created custom map

--- src/components/Location/Map.js
+++ src/components/Location/Map.js
@@ -1,21 +1,41 @@
 import *as react from 'react'
 import { useState } from 'react';
-import ReactMapGL from 'react-map-gl';
+import ReactMapGL , {Marker, NavigationControl}from 'react-map-gl';
 
 export default function Map() {
     const [viewport, setViewport] = useState({
         width: "100vw",
         height: "100vh",
-        latitude: 37.7577,
-        longitude: -122.4376,
-        zoom: 8
+        latitude: 40.7128,
+        longitude: -74.0060,
+        zoom: 4
       });
+
+      const navControlStyle= {
+        right: 10,
+        top: 10
+      };
+
+      function successLocation(position) {
+        console.log(position)
+        
+      }
+      function errorLocation() {
+        console.log("error")
+        
+      }
+
       return (
         <ReactMapGL
           {...viewport}
           mapboxApiAccessToken = {process.env.REACT_APP_MAPBOX}
+          navigator = {navigator.geolocation.getCurrentPosition(successLocation,errorLocation, {enableHighAccuracy: true})}
           onViewportChange={nextViewport => setViewport(nextViewport)}
-        />
+          mapStyle = "mapbox://styles/jamesey123/ckodjdc43157k17nnkfcn10ii"
+        >
+          <NavigationControl style={navControlStyle} />
+
+          </ReactMapGL>
       );
 
 
